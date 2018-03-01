cf4f9ae9934c620a1ffd8088d3d34314bcb22d4f
snorret <johan.ung@gmail.com>
2018-03-01 19:49:31 +0100

    Change variable name to what it is

--- boom/simple_hit_detector.py
+++ boom/simple_hit_detector.py
@@ -2,18 +2,18 @@ class SimpleHitDetector:
     def __init__(self, maxValue, hitValue):
         self.maxValue = maxValue
         self.hitValue = hitValue
-        self.lastHitValue = 0
+        self.lastHitMaxValue = 0
     
     def detect_hit(self, values):
         hits = [x for x in values if x >= self.hitValue]
         if len(hits):
-            self.lastHitValue = max(hits)
+            self.lastHitMaxValue = max(hits)
             return True
 
         return False
     
     def get_last_hit_fraction(self):
-        return self.lastHitValue / self.maxValue
+        return self.lastHitMaxValue / self.maxValue
 
     def set_hit_value(self, hitValue):
         self.hitValue = hitValue
